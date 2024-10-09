e0bb159d2abc578c09b59869b8b1d1977d7c4840
Aline Lemos <alinelemos@id.uff.br>
2024-10-09 19:49:42 -0300

    projeto1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.hello}>Hello, World!</Text>
+      <Text style={styles.emoji}>🩷</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello:{
+    fontSize: 50,
+    color: 'pink',
+    fontWeight: 'bold',
+  },
+  emoji:{
+    fontSize: 40,
+  }
 });
